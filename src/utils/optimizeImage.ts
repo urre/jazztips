7f975e5ce5ed4b5e1c081847deabfa9324544e41
Urban Sanden <hej@urre.me>
2023-03-17 09:42:40 +0100

    Refactor

--- src/utils/optimizeImage.ts
+++ src/utils/optimizeImage.ts
@@ -1,4 +1,4 @@
-const optimizeImage = (url: string) =>
+const optimizeImage = (url: string): string =>
   url.replace("upload/", "upload/q_auto,f_auto,w_500,h_500/");
 
   export default optimizeImage
